aef7403a81dc9e924a0b87cf24afe21670958f56
Kriszti <krisztina.vaci@gmail.com>
2016-06-01 16:47:55 +0200

    In one huge function except the last 3 points

--- week-05/day-3/gyak.py
+++ week-05/day-3/gyak.py
@@ -48,3 +48,65 @@ else:
     print ('No todos for today! :)')
 if sys.argv[1] == '-a' + str(input):
     print(text + (input))
+
+
+if sys.argv[1] == '-r 2':
+    print(sys.argv)
+
+
+
+def list_opener(filename):
+    f = open(filename)
+    text = f.readlines()
+    if len(sys.argv) == 1:
+        return commands(sys.argv)
+    if text != '':
+        if sys.argv[1] == '-l':
+            for i in range(len(text)):
+                print (str(i + 1) + text[i])
+    else:
+        print ('No todos for today! :)')
+    if sys.argv[1] == '-a':
+        try:
+            if sys.argv[2]:
+                print(text + sys.argv[2])
+        except:
+            raise ValueError('Unable to add: No task is provided')
+    if sys.argv[1] == '-r':
+        try:
+            if sys.argv[2] == int(2):
+                print(sys.argv)
+        except:
+            pass
+
+
+
+
+if sys.argv[1] == '-r':
+    try:
+        for n in range(len(text)):
+            if int(sys.argv[2]) - 1 == int(n):
+                text.remove(text[n])
+        print (text)
+    except:
+        print ('Unable to remove: No index is provided')
+
+
+
+if sys.argv[1] == '-a':
+    try:
+        if sys.argv[2]:
+            text.append(sys.argv[2])
+            print (text)
+    except:
+        print ('Unable to add: No task is provided')
+if sys.argv[1] == '-r' and len(sys.argv) == 3:
+    for n in range(len(text)):
+        try:
+            if int(sys.argv[2]) - 1 == int(n):
+                text.remove(text[n])
+                print (text)
+        except:
+            print('Unable to remove: Index is out of bound')
+else:
+    print ('Unable to remove: No index is provided')
