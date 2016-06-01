a39c7e0636cd39933662d46b6bf08d0a945b10cc
Kriszti <krisztina.vaci@gmail.com>
2016-06-01 13:59:16 +0200

    Empty list, add new task

--- week-05/day-3/gyak.py
+++ week-05/day-3/gyak.py
@@ -4,3 +4,47 @@ print(sys.argv)
 
 for arg in sys.argv:
     print (arg)
+
+
+
+print('Python Todo application\n'
+'=======================\n'
+
+'Command line arguments:\n'
+ '-l   Lists all the tasks\n'
+ '-a   Adds a new task\n'
+ '-r   Removes an task\n'
+ '-c   Completes an task\n')
+
+import sys
+
+def list_opener(filename):
+    f = open(filename)
+    text = f.read()
+    f.close()
+    if sys.argv != []:
+        if sys.argv[1] == '-l':
+            print (text)
+    if sys.argv == []:
+        print ('No todos for today! :)')
+
+list_opener('todos_stored.txt')
+
+if text != '':
+    if sys.argv[1] == '-l':
+        print (text)
+else:
+    print ('No todos for today! :)')
+if sys.argv[1] == '-a' + str(input):
+    print(text + (input))
+
+
+if sys.argv[1] == []:
+    return commands(sys.argv)
+elif text != '':
+    if sys.argv[1] == '-l':
+        print (text)
+else:
+    print ('No todos for today! :)')
+if sys.argv[1] == '-a' + str(input):
+    print(text + (input))
--- week-05/day-3/todo.py
+++ week-05/day-3/todo.py
@@ -1,22 +1,29 @@
+import sys, getopt
 
-print('Python Todo application\n'
-'=======================\n'
+def commands(this_filename):
+    print('Python Todo application\n'
+    '=======================\n'
 
-'Command line arguments:\n'
- '-l   Lists all the tasks\n'
- '-a   Adds a new task\n'
- '-r   Removes an task\n'
- '-c   Completes an task\n')
+    'Command line arguments:\n'
+     '-l   Lists all the tasks\n'
+     '-a   Adds a new task\n'
+     '-r   Removes a task\n'
+     '-c   Completes a task\n')
 
-import sys
 
 def list_opener(filename):
     f = open(filename)
     text = f.read()
     f.close()
-    if sys.argv[1] == '-l':
-        print (text)
-
-
+    if len(sys.argv) == 1:
+        return commands(sys.argv)
+    if text != '':
+        if sys.argv[1] == '-l':
+            print (text)
+    else:
+        print ('No todos for today! :)')
+    if sys.argv[1] == '-a':
+        if sys.argv[2]:
+            print(text + sys.argv[2])
 
 list_opener('todos_stored.txt')
