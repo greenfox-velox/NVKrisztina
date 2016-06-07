c688a47ea051b9c8e7017722d4cbe603d1921493
Kriszti <krisztina.vaci@gmail.com>
2016-06-07 13:34:19 +0200

    Character movement

--- week-06/day-2/main.py
+++ week-06/day-2/main.py
@@ -7,6 +7,10 @@ canvas.pack()
 
 photo0 = PhotoImage(file = "floor.png")
 photo1 = PhotoImage(file = "wall.png")
+photo2 = PhotoImage(file = "hero-down.png")
+photo3 = PhotoImage(file = "hero-right.png")
+photo4 = PhotoImage(file = "hero-left.png")
+photo5 = PhotoImage(file = "hero-up.png")
 # label = Label(image=photo)
 # label.image = photo
 # label.pack()
@@ -14,22 +18,66 @@ photo1 = PhotoImage(file = "wall.png")
 # canvas.create_image(0, 0, anchor = NW, image=photo0)
 # canvas.create_image(0, 0, anchor = NW, image=photo1)
 
-map1 = [[0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
-[0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
-[0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-[0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
-[0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
-[0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-[0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
-[1, 1, 1, 0, 0, 1, 0, 0, 0, 1]]
+def gameScreen():
 
-for i in range(10):
-    for j in range(10):
-        if map1[i][j] == 0:
-            canvas.create_image(i * 72, j * 72, anchor = NW, image=photo0)
-        else:
-            canvas.create_image(i * 72, j * 72, anchor = NW, image=photo1)
+    map1 = [[0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+    [1, 1, 1, 0, 0, 1, 0, 0, 0, 1]]
+
+    for i in range(10):
+        for j in range(10):
+            if map1[i][j] == 0:
+                canvas.create_image(i * 72, j * 72, anchor = NW, image = photo0)
+            else:
+                canvas.create_image(i * 72, j * 72, anchor = NW, image = photo1)
+
+gameScreen()
+
+playerposition = [0, 0]
+canvas.create_image(playerposition[0], playerposition[1], anchor = NW, image = photo2)
+
+def move_down(event):
+    gameScreen()
+    playerposition[1] = playerposition[1] + 72
+    canvas.create_image(playerposition[0], playerposition[1], anchor = NW, image = photo2)
+
+master.bind("<Down>", move_down)
+
+def move_right(event):
+    gameScreen()
+    playerposition[0] = playerposition[0] + 72
+    canvas.create_image(playerposition[0], playerposition[1], anchor = NW, image = photo3)
+
+master.bind("<Right>", move_right)
+
+def move_left(event):
+    gameScreen()
+    playerposition[0] = playerposition[0] - 72
+    canvas.create_image(playerposition[0], playerposition[1], anchor = NW, image = photo4)
+
+master.bind("<Left>", move_left)
+
+def move_up(event):
+    gameScreen()
+    playerposition[1] = playerposition[1] - 72
+    canvas.create_image(playerposition[0], playerposition[1], anchor = NW, image = photo5)
+
+master.bind("<Up>", move_up)
+
+
+
+
+
+
+
+
+
 
 master.mainloop()
