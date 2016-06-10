609bcdc2ac1344eb0c20630810efca88c3ec641a
Kriszti <krisztina.vaci@gmail.com>
2016-06-10 13:39:17 +0200

    Movements shortened, drawing unified for characters

--- week-06/day-4/main.py
+++ week-06/day-4/main.py
@@ -15,7 +15,6 @@ photo5 = PhotoImage(file = "hero-up.png")
 photo6 = PhotoImage(file = "skeleton.png")
 photo7 = PhotoImage(file = "boss.png")
 
-
 class Tile:
 
     def __init__(self, x, y, type, image):
@@ -82,7 +81,12 @@ map.getFloorCoordinates()
 
 placeTaken = []
 
-class Character:
+class Draw():
+
+    def draw(self):
+        canvas.create_image(self.playerposition[0], self.playerposition[1], anchor = NW, image = self.image)
+
+class Character():
 
     def __init__(self, playerposition, image, HP, DP, SP):
         self.playerposition = playerposition
@@ -91,13 +95,8 @@ class Character:
         self.HP = HP
         self.DP = DP
         self.SP = SP
-        self.T = Text(master, height = 2, width = 150)
+        self.T = Text(master, height = 2, width = 70)
         self.T.place(x = 5, y = 720)
-
-    def countPoints(self):
-        self.HP = random.randint(1, 6)
-        self.DP = random.randint(1, 6)
-        self.SP = random.randint(1, 6)
 
     def printPoints(self):
         self.T.insert(INSERT, 'HP: ', self.HP)
@@ -107,12 +106,17 @@ class Character:
         self.T.insert(INSERT, ' | SP: ', self.SP)
         self.T.insert(INSERT, self.SP)
 
+    def draw(self):
+        canvas.create_image(self.playerposition[0], self.playerposition[1], anchor = NW, image = self.image)
+
 class Enemy(Character):
 
     def __init__(self, playerposition, image, HP, DP, SP):
         super(Enemy, self).__init__(playerposition, image, HP, DP, SP)
+        self.randomPositionGenerator()
+        self.draw()
 
-    def draw(self):
+    def randomPositionGenerator(self):
         self.randomNumberList = []
         global placeTaken
         x = random.randint(0, 10)
@@ -120,75 +124,78 @@ class Enemy(Character):
         z = random.randint(0, 10)
         self.randomNumberList.append(z)
         if self.randomNumberList in map.getFloorCoordinates() and self.randomNumberList not in placeTaken and self.randomNumberList != [0, 0]:
-            canvas.create_image(self.randomNumberList[0] * 72, self.randomNumberList[1] * 72, anchor = NW, image = self.image)
             placeTaken = placeTaken + [self.randomNumberList]
+            self.playerposition[0] = x * 72
+            self.playerposition[1] = z * 72
         else:
-            self.draw()
-
-    def drawAnyway(self):
-        canvas.create_image(self.randomNumberList[0] * 72, self.randomNumberList[1] * 72, anchor = NW, image = self.image)
+            self.randomPositionGenerator()
 
 class Skeleton(Enemy):
 
     def __init__(self, playerposition):
-        super(Skeleton, self).__init__(playerposition, photo6, 1, 1, 1)
-        self.T.place(x = 100, y = 720)
+        super(Skeleton, self).__init__(playerposition, photo6, self.calculate() * 2, self.calculate() / 2, self.calculate())
+        self.T.place(x = 10, y = 720)
+        self.T.insert(INSERT, 'Skeleton ' )
         self.printPoints()
 
-    # def countPoints(self):
-        # self.HP = (2 * random.randint(1, 6)) + random.randint(1, 6)
-        # self.DP = (2 * random.randint(1, 6)) + (random.randint(1, 6)/2)
-        # self.SP = random.randint(1, 6)
+    def calculate(self):
+        return random.randint(1, 6)
 
 class Boss(Enemy):
 
     def __init__(self, playerposition):
-        super(Boss, self).__init__(playerposition, photo7, 1, 1, 1)
-        self.T.place(x = 300, y = 720)
+        super(Boss, self).__init__(playerposition, photo7, self.calculate() * 2 + self.calculate(), self.calculate() / 2 + self.calculate() / 2, self.calculate())
+        self.T.place(x = 500, y = 720)
+        self.T.insert(INSERT, 'Boss ' )
         self.printPoints()
 
-    # def countPoints(self):
-        # self.HP = 0
-        # self.DP = 0
-        # self.SP = 0
-
-# Hero (Level 1) HP: 8/10 | DP: 8 | SP: 6
+    def calculate(self):
+        return random.randint(1, 6)
 
 class Hero(Character):
 
     def __init__(self, playerposition):
-        super(Hero, self).__init__(playerposition, photo2, 20, 1, 5)
+        super(Hero, self).__init__(playerposition, photo2, self.calculate() * 3 + 20, self.calculate() * 2, self.calculate() + 5)
+        self.T.place(x = 300, y = 720)
+        self.T.insert(INSERT, 'Hero ' )
+        self.printPoints()
 
-    def draw(self):
-        canvas.create_image(self.playerposition[0], self.playerposition[1], anchor = NW, image = self.image)
+    def calculate(self):
+        return random.randint(1, 6)
+
+    # def battle(self):
+
+    def containAbsoltePlayerpositionParts(self):
+        absolutePlayerposition = 0
+        absolutePlayerposition = self.playerposition[0] // 72
+        return absolutePlayerposition
+
+    # def containAbsoltePlayerpositionParts1(self):
+        # output = []
+        # absolutePlayerposition = 0
+        # absolutePlayerposition = self.playerposition[0] // 72
+        # output.append(absolutePlayerposition)
+        # return absolutePlayerposition
 
     def absolutePlayerpositionUp(self):
-        absolutePlayerposition = 0
         output = []
-        absolutePlayerposition = self.playerposition[0] // 72
-        output.append(absolutePlayerposition)
+        output.append(self.containAbsoltePlayerpositionParts())
         absolutePlayerposition = self.playerposition[1] // 72
-        # if absolutePlayerposition != 0:
         absolutePlayerposition = absolutePlayerposition + 1
         output.append(absolutePlayerposition)
         return output
 
     def absolutePlayerpositionDown(self):
-        absolutePlayerposition = 0
         output = []
-        absolutePlayerposition = self.playerposition[0] // 72
-        output.append(absolutePlayerposition)
+        output.append(self.containAbsoltePlayerpositionParts())
         absolutePlayerposition = self.playerposition[1] // 72
-        # if absolutePlayerposition != 0:
         absolutePlayerposition = absolutePlayerposition - 1
         output.append(absolutePlayerposition)
         return output
 
     def absolutePlayerpositionRight(self):
-        absolutePlayerposition = 0
         output = []
-        absolutePlayerposition = self.playerposition[0] // 72
-        # if absolutePlayerposition != 0:
+        absolutePlayerposition = self.containAbsoltePlayerpositionParts()
         absolutePlayerposition = absolutePlayerposition + 1
         output.append(absolutePlayerposition)
         absolutePlayerposition = self.playerposition[1] // 72
@@ -196,95 +203,61 @@ class Hero(Character):
         return output
 
     def absolutePlayerpositionLeft(self):
-        absolutePlayerposition = 0
         output = []
-        absolutePlayerposition = self.playerposition[0] // 72
-        # if absolutePlayerposition != 0:
+        absolutePlayerposition = self.containAbsoltePlayerpositionParts()
         absolutePlayerposition = absolutePlayerposition - 1
         output.append(absolutePlayerposition)
         absolutePlayerposition = self.playerposition[1] // 72
         output.append(absolutePlayerposition)
         return output
 
+    def containMovementParts(self):
+        map.gameScreenDraw()
+        boss.draw()
+        skeleton.draw()
+        skeleton1.draw()
+        skeleton2.draw()
+        hero.draw()
+
     def move_down(self):
         if self.playerposition[1] != 9 * 72 and self.absolutePlayerpositionUp() in map.getFloorCoordinates():
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[1] = self.playerposition[1] + 72
             self.image = photo2
-            self.draw()
+            self.containMovementParts()
         else:
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[1] = self.playerposition[1]
             self.image = photo2
-            self.draw()
+            self.containMovementParts()
 
     def move_right(self):
         if self.playerposition[0] != 9 * 72 and self.absolutePlayerpositionRight() in map.getFloorCoordinates():
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[0] = self.playerposition[0] + 72
             self.image = photo3
-            self.draw()
+            self.containMovementParts()
         else:
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[0] = self.playerposition[0]
             self.image = photo3
-            self.draw()
+            self.containMovementParts()
 
     def move_left(self):
         if self.playerposition[0] != 0 and self.absolutePlayerpositionLeft() in map.getFloorCoordinates():
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[0] = self.playerposition[0] - 72
             self.image = photo4
-            self.draw()
+            self.containMovementParts()
         else:
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[0] = self.playerposition[0]
             self.image = photo4
-            self.draw()
+            self.containMovementParts()
 
     def move_up(self):
         if self.playerposition[1] != 0 and self.absolutePlayerpositionDown() in map.getFloorCoordinates():
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[1] = self.playerposition[1] - 72
             self.image = photo5
-            self.draw()
+            self.containMovementParts()
         else:
-            map.gameScreenDraw()
-            boss.drawAnyway()
-            skeleton.drawAnyway()
-            skeleton1.drawAnyway()
-            skeleton2.drawAnyway()
             self.playerposition[1] = self.playerposition[1]
             self.image = photo5
-            self.draw()
+            self.containMovementParts()
 
 def move_up_press_key(event):
     hero.move_up()
@@ -313,8 +286,6 @@ hero.draw()
 
 skeleton = Skeleton([0, 0])
 skeleton.draw()
-# skeleton.countPoints()
-# skeleton.printPoints()
 
 skeleton1 = Skeleton([0, 0])
 skeleton1.draw()
@@ -324,7 +295,5 @@ skeleton2.draw()
 
 boss = Boss([0, 0])
 boss.draw()
-# boss.countPoints()
-# boss.printPoints()
 
 master.mainloop()
