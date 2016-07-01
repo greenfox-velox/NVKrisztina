f9ce5a04f0a04b5f9b923675f36e89e19e5a8634
Kriszti <krisztina.vaci@gmail.com>
2016-07-01 11:05:12 +0200

    Thumbnails swipe left

--- week-07/day-4/gallery.js
+++ week-07/day-4/gallery.js
@@ -33,23 +33,29 @@ thumbnails.forEach(function(item, index){
 });
 
 var leftThumbnailButton = document.querySelector('.leftToThumbnail');
-leftThumbnailButton.addEventListener("click", swipeImagesLeft);
+leftThumbnailButton.addEventListener("click", swipeImagesRight);
+
+var thumbnailLength = document.querySelectorAll('.inThumbnail').length;
+
+var number = 1;
 
 function swipeImagesLeft() {
-  thumbnails.forEach(function(item, index){
-    if (index <= photoList.length){
-      item.setAttribute("src", photoList[index + 1]);
-    };
+  if (photoList.length > number + thumbnailLength - 1){
+  thumbnails.forEach(function(item, num){
+      item.setAttribute("src", photoList[num + number]);
   });
+  number += 1;
+};
 };
 
 var rightThumbnailButton = document.querySelector('.rightToThumbnail');
-rightThumbnailButton.addEventListener("click", swipeImagesRight);
+rightThumbnailButton.addEventListener("click", swipeImagesLeft);
 
 function swipeImagesRight() {
-  thumbnails.forEach(function(item, index){
-    if (index >= 0){
-    item.setAttribute("src", photoList[index - 1]);
-  };
+  if (number - 1 > 0){
+  thumbnails.forEach(function(item, num){
+    item.setAttribute("src", photoList[num - number]);
 });
+  number -= 1;
+};
 };
