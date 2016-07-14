9b7a08953c402198f1c0165ad17848b94567963e
Kriszti <krisztina.vaci@gmail.com>
2016-07-14 23:13:16 +0200

    Createoneelement is separate

--- week-08/refactored_todo/todos.js
+++ week-08/refactored_todo/todos.js
@@ -2,91 +2,45 @@
 
 var url = 'https://mysterious-dusk-8248.herokuapp.com/todos';
 
+//*****************get************************
+
 function getTodos(giveTodos) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = function() {
-    var objectRetrieved = JSON.parse(xhr.response);
-    giveTodos(objectRetrieved);
+    var response = JSON.parse(xhr.response);
+    giveTodos(response);
     };
 xhr.send();
 };
 
-function createElements(objectRetrieved){
-  objectRetrieved.forEach(function(item, index){
-    var newListItem = document.createElement('li');
-    var mainListUl = document.querySelector('ul');
-    var mainListLabel = document.createElement('label');
-    var mainListImage = document.createElement('img');
-    var mainListImage2 = document.createElement('img');
-    mainListUl.appendChild(newListItem);
-    newListItem.appendChild(mainListLabel);
-    newListItem.appendChild(mainListImage);
-    newListItem.appendChild(mainListImage2);
-    if (item.completed){
-      mainListImage2.setAttribute('src', 'tick.png');
-    } else {
-      mainListImage2.setAttribute('src', 'circle.png');
-    }
-    mainListImage2.setAttribute('type', 'checkbox');
-    mainListImage2.setAttribute('class', 'rounded_checkbox');
-    mainListImage2.addEventListener('click', check);
-    mainListImage.setAttribute('src', 'kuka.png');
-    mainListImage.addEventListener('click', remove);
-    newListItem.setAttribute('data-identifier-forremove', item.id);
-    mainListImage.setAttribute('data-identifier-forremove_image', item.id);
-    mainListImage2.setAttribute('data-identifier', item.id);
-    mainListImage2.setAttribute('data-text', item.text);
-    mainListLabel.innerHTML = item.text;
-});
+function createMoreElements(response){
+  response.forEach(function(response){
+    createOneElement(response);
+  });
 }
 
-getTodos(createElements);
+getTodos(createMoreElements);
+
+//******************add****************************
 
 var add_button = document.querySelector('button');
-add_button.addEventListener('click', add_item)
+add_button.addEventListener('click', addTodo)
 
-function add_item(){
+function addTodo(){
   var xhr = new XMLHttpRequest();
   var inputText = document.querySelector('.add_text');
   inputText = JSON.stringify({'text' : inputText.value})
   xhr.onload = function() {
-    var text = JSON.parse(xhr.responseText).text;
-    var id = JSON.parse(xhr.responseText).id;
-    createOneElement(text, id);
+    var response = JSON.parse(xhr.response);
+    createOneElement(response);
   }
   xhr.open('POST', url, true);
   xhr.setRequestHeader("content-type", "application/json; charset=utf-8");
   xhr.send(inputText);
 }
 
-function createOneElement (text, id){
-  var newListItem = document.createElement('li');
-  var mainListUl = document.querySelector('ul');
-  var mainListLabel = document.createElement('label');
-  var mainListImage = document.createElement('img');
-  var mainListImage2 = document.createElement('img');
-  mainListUl.appendChild(newListItem);
-  newListItem.appendChild(mainListLabel);
-  newListItem.appendChild(mainListImage);
-  newListItem.appendChild(mainListImage2);
-  mainListImage2.setAttribute('src', 'circle.png');
-  mainListImage2.setAttribute('type', 'checkbox');
-  mainListImage2.setAttribute('class', 'rounded_checkbox');
-  mainListImage2.addEventListener('click', check);
-  mainListImage.setAttribute('src', 'kuka.png');
-  mainListImage.addEventListener('click', remove);
-  newListItem.setAttribute('data-identifier-forremove', id);
-  mainListImage.setAttribute('data-identifier-forremove_image', id);
-  mainListImage2.setAttribute('data-identifier', id);
-  mainListImage2.setAttribute('data-text', text);
-  mainListLabel.innerHTML = text;
-};
-
-// function createMoreElements (){
-//   createOneElement()
-// }
-
+//*******************check***********************
 
 function check(event){
   var xhr = new XMLHttpRequest();
@@ -97,13 +51,14 @@ function check(event){
     if(xhr.readyState == 4 && xhr.status >= 200) {
         var addticktome = document.querySelector('[data-identifier="' + JSON.parse(xhr.response).id + '"]')
         addticktome.setAttribute('src', 'tick.PNG');
+    }
   }
-  }
-
   xhr.open('PUT', url + '/' + id);
   xhr.setRequestHeader("content-type", "application/json; charset=utf-8");
   xhr.send(text_completed);
 }
+
+//********************remove*********************
 
 function remove(event){
   var xhr = new XMLHttpRequest();
@@ -113,10 +68,38 @@ function remove(event){
         var mainListUl = document.querySelector('ul');
         var removable = document.querySelector('[data-identifier-forremove="' + JSON.parse(xhr.response).id + '"]')
         mainListUl.removeChild(removable);
+    }
   }
-  }
-
   xhr.open('DELETE', url + '/' + id);
   xhr.setRequestHeader("content-type", "application/json; charset=utf-8");
   xhr.send();
 }
+
+//*********************create one element***********
+
+function createOneElement (response){
+  var newListItem = document.createElement('li');
+  var mainListUl = document.querySelector('ul');
+  var mainListLabel = document.createElement('label');
+  var mainListImage = document.createElement('img');
+  var mainListImage2 = document.createElement('img');
+  mainListUl.appendChild(newListItem);
+  newListItem.appendChild(mainListLabel);
+  newListItem.appendChild(mainListImage);
+  newListItem.appendChild(mainListImage2);
+  if (response.completed){
+    mainListImage2.setAttribute('src', 'tick.png');
+  } else {
+    mainListImage2.setAttribute('src', 'circle.png');
+  }
+  mainListImage2.setAttribute('type', 'checkbox');
+  mainListImage2.setAttribute('class', 'rounded_checkbox');
+  mainListImage2.addEventListener('click', check);
+  mainListImage.setAttribute('src', 'kuka.png');
+  mainListImage.addEventListener('click', remove);
+  newListItem.setAttribute('data-identifier-forremove', response.id);
+  mainListImage.setAttribute('data-identifier-forremove_image', response.id);
+  mainListImage2.setAttribute('data-identifier', response.id);
+  mainListImage2.setAttribute('data-text', response.text);
+  mainListLabel.innerHTML = response.text;
+};
